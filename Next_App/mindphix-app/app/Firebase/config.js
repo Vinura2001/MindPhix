6e0df129a7d1946afa8611e525bfa730d4e88e1d
Wanninayaka Mudiyanselaage Vinura Imesh <w.m.v.imesh02@gmail.com>
2024-02-12 00:37:34 +0530

    Profile Database connection
    Profile page connect to database

--- Next_App/mindphix-app/app/Firebase/config.js
+++ Next_App/mindphix-app/app/Firebase/config.js
@@ -1,9 +1,11 @@
 import { initializeApp, getApps, getApp} from "firebase/app";
 import {getAuth} from 'firebase/auth'
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCvd1Lm0ePAj-fgc4npTy6SuUBN3jFp-IE",
   authDomain: "mindphix-4737c.firebaseapp.com",
+  databaseURL: "https://mindphix-4737c-default-rtdb.asia-southeast1.firebasedatabase.app/",
   projectId: "mindphix-4737c",
   storageBucket: "mindphix-4737c.appspot.com",
   messagingSenderId: "732626597375",
@@ -14,5 +16,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
 const auth = getAuth(app)
+const database = getDatabase(app);
 
-export {app, auth}
+export {app, auth, database}
--- Next_App/mindphix-app/app/dashboard/Profile/page.tsx
+++ Next_App/mindphix-app/app/dashboard/Profile/page.tsx
@@ -1,58 +1,101 @@
+"use client";
 import Link from "next/link";
 import BaseLayout from "../BaseLayout";
 
 
+import {get, ref,} from 'firebase/database';
+import { useState, useEffect } from 'react';
+import { database } from "@/app/firebase/config";
+
+
+
+interface User {
+  id: string;
+  First_Name: string;
+  Last_Name: string;
+  User_Name: string;
+  Date_of_Birth: string;
+  Gender: string; 
+  Email_Address: string;
+  Phone_Number: string;
+  Status: string;
+}
+
 export default function Profile() {
+
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const usersRef = ref(database, 'users');
+    get(usersRef).then((snapshot) => {
+      if (snapshot.exists()) {
+        const usersArray: User[] = Object.entries(snapshot.val()).map(([id, data]: [string, any]) => ({
+          id,
+          ...data,
+        }));
+        setUsers(usersArray);
+      } else {
+        console.log('No data available')
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+  }, []);
+
   return(
     <BaseLayout>
-      <div>
-      {/*User Profile content Start*/}
-
-        <h1 className="UserProfile_Topic">User Profile</h1>
-
-        <div className="UserProfile_TopBox">
-            <img className="UserProfile_TopBox_Image" src="/Figures/UserProfileTopBoxImage.png" alt="TopBox_Image" />
-            <div className="Name">Steven Watson</div>
-
-            <div className="TopBoxUserImage">
-                <img className="UserImage_TopBox" src="/Figures/UserImage_TopBox.png" alt="UserImage" />
+      {users.map((user) => (
+          <div key={user.id}>
+          {/*User Profile content Start*/}
+    
+            <h1 className="UserProfile_Topic">User Profile</h1>
+    
+            <div className="UserProfile_TopBox">
+                <img className="UserProfile_TopBox_Image" src="/Figures/UserProfileTopBoxImage.png" alt="TopBox_Image" />
+                
+                <div className="Name">{user.First_Name} {user.Last_Name}</div>
+                
+                <div className="TopBoxUserImage">
+                    <img className="UserImage_TopBox" src="/Figures/UserImage_TopBox.png" alt="UserImage" />
+                </div>
+    
+                <Link href='/dashboard/Profile/EditProfile'>
+                  <button className="EditProfile_Button">Edit Profile</button>
+                </Link>
+    
             </div>
-
-            <Link href='/dashboard/Profile/EditProfile'>
-              <button className="EditProfile_Button">Edit Profile</button>
-            </Link>
-
-        </div>
-
-        {/*Personal Information Start*/}
-        <div className="PersonalInformationBox">
-            <div className="PersonalInformation_Topic">Personal Information</div>
-
-            <div className="FirstName_heading">First Name</div>
-            <div className="FirstName">Steven</div>
-
-            <div className="LastName_heading">Last Name</div>
-            <div className="LastName">Watson</div>
-
-            <div className="UserName_heading">User Name</div>
-            <div className="UserName">StevenWatson</div>
-
-            <div className="DOB_heading">Date of Birth</div>
-            <div className="DOB">1999/05/10</div>
-
-            <div className="Gender_heading">Gender</div>
-            <div className="Gender">Male</div>
-
-            <div className="EmailAddress_heading">Email Address</div>
-            <div className="EmailAddress">stevenwatson99@gmail.com</div>
-
-            <div className="PhoneNumber_heading">Phone Number</div>
-            <div className="PhoneNumber">+94 770975126</div>
-
-            <div className="Status_heading">Status</div>
-            <div className="Status">Active</div>
-        </div>
-      </div>
+    
+            {/*Personal Information Start*/}
+            <div className="PersonalInformationBox">
+                <div className="PersonalInformation_Topic">Personal Information</div>
+    
+                <div className="FirstName_heading">First Name</div>
+                <div className="FirstName">{user.First_Name}</div>
+    
+                <div className="LastName_heading">Last Name</div>
+                <div className="LastName">{user.Last_Name}</div>
+    
+                <div className="UserName_heading">User Name</div>
+                <div className="UserName">{user.User_Name}</div>
+    
+                <div className="DOB_heading">Date of Birth</div>
+                <div className="DOB">{user.Date_of_Birth}</div>
+    
+                <div className="Gender_heading">Gender</div>
+                <div className="Gender">{user.Gender}</div>
+    
+                <div className="EmailAddress_heading">Email Address</div>
+                <div className="EmailAddress">{user.Email_Address}</div>
+    
+                <div className="PhoneNumber_heading">Phone Number</div>
+                <div className="PhoneNumber">{user.Phone_Number}</div>
+    
+                <div className="Status_heading">Status</div>
+                <div className="Status">{user.Status}</div>
+            </div>
+          </div>    
+      ))}
+      
     </BaseLayout>
   );
 }
