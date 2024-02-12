c5dc38546c9628110f46cd5967578e87d2b45b3c
Dileesha Gamage <dilligamage21@gmail.com>
2024-02-12 22:07:17 +0530

    login-validate
    add validation to user credentials, make both login and signup pages responsive, add google authentication

--- Next_App/mindphix-app/app/sign-in/signin.tsx
+++ Next_App/mindphix-app/app/sign-in/signin.tsx
@@ -1,16 +1,12 @@
 'use client'
-import Head from "next/head";
+
 import Image from "next/image";
 import background from "@/public/login-assets/Login.jpg";
-import {
-  FaFacebookF,
-  FaLinkedinIn,
-  FaGoogle,
-  FaRegEnvelope,
-} from 'react-icons/fa';
+import { FaRegEnvelope } from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc';
 import { MdLockOutline } from 'react-icons/md';
 import Link from "next/link";
-import { useState } from "react";
+import { useState, ChangeEvent } from "react";
 import {auth} from '@/app/firebase/config';
 import {useSignInWithEmailAndPassword,useSignInWithGoogle} from 'react-firebase-hooks/auth';
 import React from "react";
@@ -21,37 +17,52 @@ export default function Signin() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const[signInWithEmailAndPassword] = useSignInWithEmailAndPassword(auth);
+  const[signInWithGoogle] = useSignInWithGoogle(auth);
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
   
-   
-  const handleSignIn = async () => {
-    try{
-      // Firebase createUserWithEmailAndPassword
-      const res = await signInWithEmailAndPassword(email, password);
-      setEmail('');
-      setPassword('');
-
-       // Redirect upon successful signup
-       if (res) {
-      
-        router.push("/dashboard/Dashboard");
+  const handleSignInWithEmailAndPassword = async () => {
+    try {
+      if (!email || !password) {
+        setEmailError("Email is required.");
+        setPasswordError("Password is required.");
+        return;
       }
 
-     
-    } catch (e) {
-      console.error(e);
+      const res = await signInWithEmailAndPassword(email, password);
+
+      if (res) {
+        router.push("/dashboard/Dashboard");
+      }
+    } catch (error) {
+      console.error("Error signing in:", error);
     }
+  };
+
+  const handleSignInWithGoogle = async () => {
+    try {
+      await signInWithGoogle();
+      router.push("/dashboard/Dashboard");
+    } catch (error) {
+      console.error("Error signing in with Google:", error);
+    }
+  };
+
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+    setEmailError("");
+  };
+
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+    setPasswordError("");
   };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100">
-      <Head>
-        <title>Create Signin App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main className="flex flex-col items-center justify-center w-full flex-1 px-5 sm:px-20 text-center">
-        <div className="bg-white rounded-2xl shadow-2xl flex max-w-4xl w-3/4">
-          <div className="w-3/5 p-5 text-black ml-10 mr-10">
+        <div className="bg-white rounded-2xl shadow-2xl flex flex-col sm:flex-row w-full max-w-4xl overflow-hidden">
+          <div className="w-full sm:w-3/5 p-5 text-black">
             <div className="text-left font-bold text-black">
               <span className="text-blue-400">Mind</span>Phix
             </div>
@@ -59,71 +70,58 @@ export default function Signin() {
               <h2 className="text-3xl font-bold mb-2">Sign into account</h2>
               <div className="border-2 w-10 border-blue-500 inline-block mb-2"></div>
               <div className="flex justify-center my-2">
-                <a
-                  href="#"
-                  className="border-2 border-gray-200 rounded-full p-3 mx-1"
-                >
-                  <FaFacebookF className="text-s" />
-                </a>
-                <a
-                  href="#"
-                  className="border-2 border-gray-200 rounded-full p-3 mx-1"
-                >
-                  <FaLinkedinIn className="text-s" />
-                </a>
-                <a
-                  href="#"
-                  className="border-2 border-gray-200 rounded-full p-3 mx-1"
-                >
-                  <FaGoogle className="text-s" />
-                </a>
+                <button 
+                className="border-2 border-gray-200 text-sm font-semibold text-gray-400 rounded-full px-12 py-2 inline-flex items-center hover:bg-gray-200 hover:text-black mb-2"
+                onClick = {handleSignInWithGoogle} >
+                  <FcGoogle className="mr-2"/>
+                  <span>Sign in via your Google Account</span>
+                </button>
               </div>
               {/*Other login methods*/}
+
               <p className="font-semibold text-gray-400 my-3">
                 or use your email account
               </p>
               <div className="flex flex-col items-center">
-                <div className="bg-gray-100 w-64 p-2 flex items-center mb-3">
+                <div className="bg-gray-100 w-full sm:w-64 p-2 flex items-center mb-3">
                   <FaRegEnvelope className="text-gray-400 mr-2" />
                   <input
                     type="email"
                     name="email"
                     placeholder="Email address"
                     className="bg-gray-100 outline-none text-sm flex-1"
-                    onChange={(e) => setEmail(e.target.value)}
+                    onChange={handleEmailChange}
                     value={email}
-                    required
-                    
                   />
                 </div>
-                <div className="bg-gray-100 w-64 p-2 flex items-center mb-3">
+                {emailError && <p className="text-red-500 text-xs font-semibold mb-3 ">{emailError}</p>}
+                <div className="bg-gray-100 w-full sm:w-64 p-2 flex items-center mb-3">
                   <MdLockOutline className="text-gray-400 mr-2" />
                   <input
                     type="password"
                     name="password"
                     placeholder="Password"
                     className="bg-gray-100 outline-none text-sm flex-1"
-                    onChange={(e) => setPassword(e.target.value)}
+                    onChange={handlePasswordChange}
                     value={password}
-                    required
                   />
                 </div>
-                <div className="flex justify-between w-64 mb-5">
-                  <label className="flex items-center text-xs">
-                    <input type="checkbox" name="remember" className="mr-1 " />
-                    Remember me
-                  </label>
+                {passwordError && <p className="text-red-500 text-xs font-semibold">{passwordError}</p>}
+
+            
+
+                <div className="flex justify-end w-64 mt-2 mb-5">
                   <Link
                     href="/sign-in/forget-password"
                     className="text-xs font-semibold"
                   >
-                    Forgot Password?
+                    Forget Your Password?
                   </Link>
                 </div>
 
                 <button
                   className=" border-2 border-blue-400 font-semibold text-blue-500 rounded-full px-12 py-2 inline-block hover:bg-blue-400 hover:text-white mb-5"
-                  onClick={handleSignIn} 
+                  onClick={handleSignInWithEmailAndPassword} 
                 >
                   Login
                 </button>
@@ -136,13 +134,15 @@ export default function Signin() {
           </div>
           {/*Signin section*/}
 
-          <Image
-            className="w-3/4 rounded-tr-2xl rounded-br-2xl"
+          <div className="hidden sm:block w-2/5 relative">
+            <Image
+            className="rounded-tr-2xl rounded-br-2xl"
             src={background}
             alt="background image"
-            width={400}
-            height={1000}
+            layout="fill"
+            objectFit="cover"
           />
+          </div>
 
           {/*Signup section*/}
         </div>
--- Next_App/mindphix-app/app/sign-up/signup.tsx
+++ Next_App/mindphix-app/app/sign-up/signup.tsx
@@ -2,11 +2,12 @@
 import Head from "next/head";
 import Image from "next/image";
 import background from "@/public/login-assets/Signup.jpg";
-import { FaRegEnvelope, FaRegUser } from "react-icons/fa";
+import { FaRegEnvelope } from "react-icons/fa";
 import { MdLockOutline } from "react-icons/md";
 import Link from "next/link";
-import { useState } from "react";
-import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import{ FcGoogle } from 'react-icons/fc';
+import { useState, ChangeEvent } from "react";
+import { useCreateUserWithEmailAndPassword, useSignInWithGoogle } from "react-firebase-hooks/auth";
 import { auth } from "@/app/firebase/config";
 import { useRouter } from "next/navigation";
 import React from "react";
@@ -19,44 +20,80 @@ export default function Signup() {
   const [emailError, setEmailError] = React.useState("");
   const [passwordError, setPasswordError] = React.useState("");
   const [confirmPasswordError, setConfirmPasswordError] = React.useState("");
-
-  const [createUserWithEmailAndPassword] =
-    useCreateUserWithEmailAndPassword(auth);
+  const[signInWithGoogle] = useSignInWithGoogle(auth);
+  const [createUserWithEmailAndPassword] = useCreateUserWithEmailAndPassword(auth);
 
   const handleSignUp = async () => {
     try {
-      // Firebase createUserWithEmailAndPassword
+      if (!email || !password || !confirmPassword) {
+        setEmailError("Please enter your user details to proceed.");
+        return;
+      }
+
+      if (password !== confirmPassword) {
+        setConfirmPasswordError("Passwords don't match with each.");
+        return;
+      }
+
       const res = await createUserWithEmailAndPassword(email, password);
 
-      // Redirect upon successful signup
       if (res) {
         router.push("/dashboard/Dashboard");
       }
-    } catch (e) {
-      console.error(e);
+    } catch (error) {
+      console.error("Error signing up:", error);
     }
   };
+
+  const handleSignInWithGoogle = async () => {
+    try {
+      await signInWithGoogle();
+      router.push("/dashboard/Dashboard");
+    } catch (error) {
+      console.error("Error signing in with Google:", error);
+    }
+  };
+
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+    setEmailError("");
+  };
+
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+    setPasswordError("");
+  };
+
+  const handleConfirmPasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setConfirmPassword(e.target.value);
+    setConfirmPasswordError("");
+  };
+  
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100">
-      <Head>
-        <title>Create SignUP App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main className="flex flex-col items-center justify-center w-full flex-1 px-5 sm:px-20 text-center">
-        <div className="bg-white rounded-2xl shadow-2xl flex max-w-4xl w-3/4">
-          <div className="w-3/5 p-5 text-black ml-10 mr-10">
+        <div className="bg-white rounded-2xl shadow-2xl flex flex-col sm:flex-row w-full max-w-4xl overflow-hidden">
+          <div className="w-full sm:w-3/5 p-5 text-black">
             <div className="text-left font-bold text-black">
               <span className="text-blue-400">Mind</span>Phix
             </div>
             <div className="py-10">
               <h2 className="text-3xl font-bold mb-2">Create a new account</h2>
               <div className="border-2 w-10 border-blue-500 inline-block mb-2"></div>
-
+              <div className="flex justify-center my-2">
+                <button 
+                  className="border-2 border-gray-200 text-sm font-semibold text-gray-400 rounded-full px-12 py-2 inline-flex items-center hover:bg-gray-200 hover:text-black mb-2"
+                  onClick={handleSignInWithGoogle}
+                >
+                  <FcGoogle className="mr-2"/>
+                  <span>Sign in via your Google Account</span>
+                </button>
+              </div>
               <p className="font-semibold text-gray-400 my-3">
                 Enter your user credentials here{" "}
               </p>
               <div className="flex flex-col items-center">
+              {emailError && <p className="text-red-500 text-xs font-semibold mb-3">{emailError}</p>}
                 <div className="bg-gray-100 w-full sm:w-64 p-2 flex items-center mb-3">
                   <FaRegEnvelope className="text-gray-400 mr-2" />
                   <input
@@ -64,10 +101,11 @@ export default function Signup() {
                     name="email"
                     placeholder="Email address"
                     className="bg-gray-100 outline-none text-sm flex-1"
-                    onChange={(e) => setEmail(e.target.value)}
+                    onChange={handleEmailChange}
                     value={email}
                   />
                 </div>
+                
                 <div className="bg-gray-100 w-full sm:w-64 p-2 flex items-center mb-3">
                   <MdLockOutline className="text-gray-400 mr-2" />
                   <input
@@ -75,10 +113,11 @@ export default function Signup() {
                     name="password"
                     placeholder="Password"
                     className="bg-gray-100 outline-none text-sm flex-1"
-                    onChange={(e) => setPassword(e.target.value)}
+                    onChange={handlePasswordChange}
                     value={password}
                   />
                 </div>
+                {passwordError && <p className="text-red-500 text-xs font-semibold ">{passwordError}</p>}
                 <div className="bg-gray-100 w-full sm:w-64 p-2 flex items-center mb-3">
                   <MdLockOutline className="text-gray-400 mr-2" />
                   <input
@@ -86,13 +125,14 @@ export default function Signup() {
                     name="cpassword"
                     placeholder="Confirm Password"
                     className="bg-gray-100 outline-none text-sm flex-1"
-                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    onChange={handleConfirmPasswordChange}
                     value={confirmPassword}
                   />
                 </div>
+                {confirmPasswordError && <p className="text-red-500 text-xs font-semibold mb-3">{confirmPasswordError}</p>}
                 <button
                   onClick={handleSignUp}
-                  className=" border-2 border-blue-400 font-semibold text-blue-500 rounded-full px-12 mt-2 py-2 inline-block hover:bg-blue-400 hover:text-white mb-5"
+                  className=" border-2 border-blue-400 font-semibold text-blue-500 rounded-full px-12 py-2 inline-block hover:bg-blue-400 hover:text-white mb-5"
                 >
                   Signup
                 </button>
@@ -102,18 +142,18 @@ export default function Signup() {
               </div>
             </div>
           </div>
-          {/*Signup section*/}
-          <Image
-            className="rounded-tr-2xl rounded-br-2xl w-3/4"
-            src={background}
-            alt="background image"
-            width={400}
-            height={500}
-          />
-
-          {/*Signin section*/}
+          <div className="hidden sm:block w-full sm:w-2/5 relative">
+            <Image
+              className="rounded-tr-2xl rounded-br-2xl "
+              src={background}
+              alt="background image"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
         </div>
       </main>
     </div>
   );
+
 }
